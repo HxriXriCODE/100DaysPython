7291c9cd0b5476680f0828caec3e22c0244a5c7e
Dev_HariKrishna <harikrishna83830@gmail.com>
2025-07-26 19:27:53 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,29 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import random
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+Let=[]
+for letter in range(0,nr_letters):
+    Let+=random.choice(letters)
+Num=[]
+for number in range(0,nr_numbers):
+    Num+=random.choice(numbers)
+Sym=[]
+for symbol in range(0,nr_symbols):
+    Sym+=random.choice(symbols)
+Collect=Let+Num+Sym
+key=""
+for password in Collect:
+    key+=random.choice(Collect)
+print(key)
+
+
+
+
+
