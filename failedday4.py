a80c66d82222a367b259808b2889e22705f1ed44
Dev_HariKrishna <harikrishna83830@gmail.com>
2025-08-01 22:45:02 +0530

    Add files via upload
    it's been 4 day I not solved a real world problem

---
+++ failedday4.py
@@ -0,0 +1,11 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+Max=0
+for studentMax in student_scores:
+    if studentMax > Max :
+        Max = studentMax
+print(Max)
+
+
+
+
